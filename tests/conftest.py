ee6d73fbbf1e3c7eba12e1d6091da11b79bae176
Vladislav Borisov <93284707+yanestabilniypomogite@users.noreply.github.com>
2022-11-03 13:46:50 +0300

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,12 +16,11 @@ def browser():
     """
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("start-maximized") # open Browser in maximized mode
-    chrome_options.add_argument("--disable-infobars") # disabling infobars
-    chrome_options.add_argument("--disable-extensions") # disabling extensions
-    chrome_options.add_argument("--disable-gpu") #  applicable to windows os only
-    chrome_options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu") 
+    chrome_options.add_argument("--disable-dev-shm-usage") 
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
--- tests/test_first.py
+++ tests/test_first.py
@@ -9,31 +9,21 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def test_product_view_sku():
+def test_product_view_sku(browser):
     """
     Test case WERT-1
     """
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("start-maximized")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
     url = "https://test.qa.studio"
-    driver.get(url=url)
+    browser.get(url=url)
 
-    element = driver.find_element(by=By.CSS_SELECTOR, value="[class='tab-featured ']")
+    element = browser.find_element(by=By.CSS_SELECTOR, value="[class='tab-featured ']")
     element.click()
 
     x_path_table = '//*[@id="rz-shop-content"]/ul/li[1]/div/div[2]/h2/a'
-    element = driver.find_element(By.XPATH, value=x_path_table)
+    element = browser.find_element(By.XPATH, value=x_path_table)
     element.click()
 
-    sku = driver.find_element(By.CLASS_NAME, value="sku")
+    sku = browser.find_element(By.CLASS_NAME, value="sku")
 
     assert sku.text == 'C0MSSDSUM7', "Unexpected sku"
 
